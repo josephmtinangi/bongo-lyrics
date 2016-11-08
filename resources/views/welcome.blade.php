37857aa9ebd18e94713d0ca40e1b63e52eb7087e
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-08 10:11:06 +0300

    Configure app name and welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bongo Lyrics
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>
+                    Discover music lyrics for your favorite artists.
+                </p>
             </div>
         </div>
     </body>
