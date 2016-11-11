6a29ff9f6d3dd9efaff79a8735bca8cdbf22f4bb
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 18:13:17 +0300

    Show all genres

--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -1,10 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Genres</h1>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
+
+                <h2 class="text-center">Recently Added</h2>
+
+                @if($genres->count() > 0)
+                    @foreach($genres->chunk(4) as $genresSet)
+                        <div class="row">
+                            @foreach($genresSet as $genre)
+                                <div class="col-sm-3">
+                                    <div class="thumbnail">
+                                        <img src="#" alt="">
+                                        <div class="caption">
+                                            <h3>{{ $genre->display_name }}</h3>
+                                            <p>
+                                                <a href="{{ url('genres/' . $genre->id) }}" class="btn btn-primary">Songs</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endforeach
+                @else
+                    No Genre
+                @endif
+
             </div>
         </div>
     </div>
