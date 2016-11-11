34393d640ae884023120198bee0f45391c0e82a0
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 17:07:28 +0300

    Add headers to login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <h2>Login into your account</h2>
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
