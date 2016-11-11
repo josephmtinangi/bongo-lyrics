1fb4d9dfeb5fabae037f205f1b7992f31303665b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 17:03:23 +0300

    Show footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+        <p class="text-center">
+            Copyright &copy {{ date('Y') }} Bongo Lyrics. All Rights Reserved | Developed by <a
+                    href="https://josephmtinangi.github.io" target="_blank">Joseph Mtinangi</a>
+        </p>
+    </div>
+</footer>
