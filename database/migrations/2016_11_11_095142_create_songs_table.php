eb70182a567ff914f84cf66511c3f08095ff5ce5
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 12:57:14 +0300

    Add fields to songs table

--- database/migrations/2016_11_11_095142_create_songs_table.php
+++ database/migrations/2016_11_11_095142_create_songs_table.php
@@ -15,6 +15,11 @@ class CreateSongsTable extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->integer('artist_id')->unsigned();
+            $table->foreign('artist_id')->references('id')->on('artists')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('song_url')->nullable();
+            $table->text('lyrics');
             $table->timestamps();
         });
     }
