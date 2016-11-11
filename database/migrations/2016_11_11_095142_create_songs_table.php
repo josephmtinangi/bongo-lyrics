7b0df49be5a17177a2d6fdbba120ba90c272a95d
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 13:17:25 +0300

    Add genre_id field to songs table

--- database/migrations/2016_11_11_095142_create_songs_table.php
+++ database/migrations/2016_11_11_095142_create_songs_table.php
@@ -20,6 +20,8 @@ class CreateSongsTable extends Migration
             $table->foreign('artist_id')->references('id')->on('artists')->onUpdate('cascade')->onDelete('cascade');
             $table->string('youtube_url')->nullable();
             $table->text('lyrics');
+            $table->integer('genre_id')->unsigned();
+            $table->foreign('genre_id')->references('id')->on('genre')->onUpdate('cascade')->onDelete('cascade');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
