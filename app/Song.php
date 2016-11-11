d9b7538e69e93970910d3e0c6d1edb6c8d5bc39f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-11 13:19:49 +0300

    Add fillables to Song model

--- app/Song.php
+++ app/Song.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'artist_id',
+        'youtube_url',
+        'lyrics',
+        'genre_id',
+        'user_id',
+    ];
 }
