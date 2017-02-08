df9db462ea8d90dd344964003a9016472bff33bc
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-08 16:48:22 +0300

    Add link to a single lyric page

--- resources/views/lyrics/_lyrics.blade.php
+++ resources/views/lyrics/_lyrics.blade.php
@@ -8,7 +8,7 @@
                                 <span class="glyphicon glyphicon-music lyric-icon"></span>
                             </div>
                             <div class="caption">
-                                <h3>{{ str_limit($lyric->title, 37) }}</h3>
+                                <h3><a href="{{ url('lyrics/'.$lyric->id) }}">{{ str_limit($lyric->title, 37) }}</a></h3>
                             </div>
                         </div>
                 </div>
