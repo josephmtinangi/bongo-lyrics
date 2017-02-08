47bb025bf1f7a4069e3392ee8db69f44aaef8563
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-08 16:25:19 +0300

    Add jumbotron to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="jumbotron">
+	<div class="container text-center">
+		<h1>Bongo Lyrics</h1>
+		<p>
+			Rifle franchise tube shanty town jeans market render-farm construct spook shrine decay sunglasses cardboard fetishism apophenia monofilament ablative. 
+		</p>
+		<p>
+			<a href="{{ url('register') }}" class="btn btn-primary btn-lg">Get Started</a>
+		</p>
+	</div>
+</div>
+
     <div class="container">
         <div class="recently">
             <h1 class="text-center">Recently Added</h1>
