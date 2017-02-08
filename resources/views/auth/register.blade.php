231f43315d8fab8d6fbfedfc3e1f6393ad8ac894
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-08 18:21:24 +0300

    Style login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,16 +4,17 @@
     <div class="register">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <h2 class="text-center">Register for free</h2>
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Register for free</h3>
+                        </div>
+                        <div class="panel-body">
+                    <form role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name"
+                                <input id="name" type="text" placeholder="Name" class="form-control" name="name"
                                        value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -21,14 +22,10 @@
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email"
+                                <input id="email" type="email" class="form-control" placeholder="Email" name="email"
                                        value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -36,28 +33,23 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" placeholder="Password" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control"
+                                <input id="password-confirm" type="password" placeholder="Confirm Password" class="form-control"
                                        name="password_confirmation" required>
                             </div>
                         </div>
@@ -70,6 +62,8 @@
                             </div>
                         </div>
                     </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
